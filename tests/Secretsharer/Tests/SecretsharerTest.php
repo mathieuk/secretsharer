8cc1001a58579a7afd7edf4ddb6a5472dcf9f1a6
Mathieu Kooiman <mathieuk@gmail.com>
2016-01-03 16:12:27 +0100

    Add a few unit tests

--- tests/Secretsharer/Tests/SecretsharerTest.php
+++ tests/Secretsharer/Tests/SecretsharerTest.php
@@ -2,28 +2,60 @@
 
 namespace Secretsharer\Tests;
 
-use Secretsharer;
+use Secretsharer\PlaintextSecretSharer;
 
 class SecretsharerTest extends \PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        $this->_sharer = new Secretsharer();
+        $this->_sharer = new PlaintextSecretSharer();
     }
     
-    public function testSharesCannotHaveDashes()
-    {   
-    }
-    
-    public function testSecretMaxLengthIs128Bytes()
+    /**
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Share should have exactly one '-'
+     */
+    public function testProvidedSharesCannotHaveDashes()
     {
+        $this->_sharer->recoverSecret([
+            '1--1234',
+            '2-abcdef'
+        ]);
     }
     
-    public function testSecretSharerCreates2SharesWhichRecoverToTheInitialSecret()
+    public function testSecretSharerCanSplit91ByteSecrets()
     {   
+        $secret = str_repeat("a", 91);
+        $shares = $this->_sharer->splitSecret($secret, 2, 4);
+        
+        $this->assertEquals(4, count($shares), "Secretsharer should've returned 4 shares");
+        $recoveredSecret = $this->_sharer->recoverSecret([$shares[0], $shares[3]]);
+        
+        $this->assertEquals($secret, $recoveredSecret);
+    }
+    
+    
+    public function testSecretSharerRecoversSecretFrom2Shares()
+    {   
+        $secret = "ABCDEF";
+        $shares = $this->_sharer->splitSecret($secret, 2, 4);
+        
+        $this->assertEquals(4, count($shares), "Secretsharer should've returned 4 shares");
+        $recoveredSecret = $this->_sharer->recoverSecret([$shares[0], $shares[3]]);
+        
+        $this->assertEquals($secret, $recoveredSecret);
     }
     
     public function testSecretSharerRecoversSecretFrom3Shares()
     {
+        $secret = "ABCDEF";
+        $shares = $this->_sharer->splitSecret($secret, 3, 6);
+        
+        $this->assertEquals(6, count($shares), "Secretsharer should've returned 4 shares");
+        $recoveredSecret = $this->_sharer->recoverSecret([$shares[0], $shares[5], $shares[3]]);
+        
+        $this->assertEquals($secret, $recoveredSecret);
     }
+    
+    
 }
